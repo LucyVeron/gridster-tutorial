ed4e733d412a6353bc86b784d32d44b365402bda
Veron <fiercebrosnan@protonmail.com>
2020-05-30 21:31:41 +0200

    [WIP] Step C: Implement full gridster functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DisplayGrid, GridsterConfig, GridType } from 'angular-gridster2';
 import { AddCardDialogComponent } from './add-card-dialog/add-card-dialog.component';
 
 @Component({
@@ -7,14 +8,32 @@ import { AddCardDialogComponent } from './add-card-dialog/add-card-dialog.compon
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public card: any;
+  public options: GridsterConfig;
+  public dashboard: Array<any>;
 
   constructor(public dialog: MatDialog) { }
 
+  ngOnInit() {
+    this.options = {
+      gridType: GridType.Fixed,
+      /* compactType: CompactType.CompactLeftAndUp, */
+      displayGrid: DisplayGrid.None,
+      fixedColWidth: 200,
+      fixedRowHeight: 200,
+      draggable: { enabled: true }
+    };
+    this.dashboard = [];
+  }
+
   public addCard(): void {
     const dialogRef = this.dialog.open(AddCardDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
+    dialogRef.afterClosed().subscribe((config: any) => {
+      if (!config) { return; }
+      this.dashboard.push({ config, cols: 1, rows: 1, y: 0, x: 0 });
+      console.log({ config, cols: 1, rows: 1, y: 0, x: 0 });
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridsterModule } from 'angular-gridster2';
 import { AddCardDialogComponent } from './add-card-dialog/add-card-dialog.component';
 import { AppComponent } from './app.component';
 
@@ -17,11 +24,19 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GridsterModule,
     MatToolbarModule,
     MatCardModule,
     MatIconModule,
     MatRippleModule,
-    MatDialogModule
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule
   ],
   entryComponents: [
     AddCardDialogComponent
